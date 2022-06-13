055cc9309e4bb3617421c538ffa7e1ac54224492
xxxhakuaxxx <naoki.06110201@gmail.com>
2022-06-13 14:55:38 +0900

    入れポン

---
+++ src/test.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class test {
+    public static void main ( String[] args ) {
+
+        // 接続文字列の設定
+        String url = "jdbc:postgresql://localhost:5432/testdb" ;
+        String user = "postgres" ;
+        String password = "pass" ;
+
+        // SELECT文の作成・実行
+        String sql = "SELECT * from public.keyakizaka" ;
+
+        // PostgreSQLに接続
+        try (Connection con = DriverManager.getConnection ( url, user, password );
+                Statement stmt = con.createStatement();
+                ResultSet result = stmt.executeQuery ( sql ); ) {
+
+            // 実行結果の取得
+            while ( result.next() ) {
+                String col1 = result.getString ( 1 ) ;
+                String col2 = result.getString ( 2 ) ;
+                String col3 = result.getString ( 3 ) ;
+                String col4 = result.getString ( 4 ) ;
+                String col5 = result.getString ( 5 ) ;
+                System.out.println ( col1 + " " + col2 + " " + col3 + " " + col4 + " " + col5) ;
+            }
+        } catch ( SQLException e ) {
+          e.printStackTrace() ;
+      }
+    }
+}
